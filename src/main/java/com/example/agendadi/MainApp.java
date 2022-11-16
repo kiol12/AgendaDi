82bf0b92316ef257b6c422aa14c3a49b3418870a
kiol12 <james2002gs@gmail.com>
2022-11-16 11:42:57 +0100

    Hasta el paso 4

--- src/main/java/com/example/agendadi/MainApp.java
+++ src/main/java/com/example/agendadi/MainApp.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -42,6 +43,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        this.primaryStage.getIcons().add(new Image("file:src/main/resources/com/example/agendadi/images/9036037_rocket_sharp_icon.png"));
 
         initRootLayout();
 
